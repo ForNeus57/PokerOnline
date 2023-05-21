cc289ebc5020edef09d845a82763438951908e28
ForNeus57 <dominikbreksa@student.agh.edu.pl>
2023-05-21 19:37:21 +0200

    Suit tests 40%

---
+++ Model/lib/internal/include/Model/rank.h
@@ -0,0 +1,10 @@
+//
+// Created by Dominik on 19.05.2023.
+//
+
+#ifndef POKER_ONLINE_RANK_H
+#define POKER_ONLINE_RANK_H
+
+
+
+#endif //POKER_ONLINE_RANK_H
--- Model/main.cpp
+++ Model/main.cpp
@@ -1,10 +1,13 @@
 //
 // Created by Dominik on 10.05.2023.
 //
-#include <Common/automata.h>
-#include <Model/card.h>
-#include <boost/thread.hpp>
+#include <Model/suit.h>
 
 int main() {
+	
+	poker::Suit s1{};
+	poker::Suit s2(poker::Suit::Value::Pick);
+	std::cout << (s1 < s2) << std::endl;
+	
 	return EXIT_SUCCESS;
 }
