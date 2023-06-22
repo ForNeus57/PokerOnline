6a4b2b1c2fac79817936a92c2e9122b920bd9420
ForNeus57 <dominikbreksa@student.agh.edu.pl>
2023-06-22 16:41:37 +0200

    Database connection mamagement simple skeleton

---
+++ Server/lib/internal/include/Server/Database/database_thread.h
@@ -0,0 +1,8 @@
+//
+// Created by Dominik on 22.06.2023.
+//
+
+#ifndef POKER_ONLINE_DATABASE_THREAD_H
+#define POKER_ONLINE_DATABASE_THREAD_H
+
+#endif //POKER_ONLINE_DATABASE_THREAD_H
--- Server/test/src/database_manager_test.cpp
+++ Server/test/src/database_manager_test.cpp
@@ -2,7 +2,7 @@
 // Created by Dominik on 10.05.2023.
 //
 #include <gtest/gtest.h>
-#include <Server/database_manager.h>
+#include "Server/Database/database_manager.h"
 
 TEST(ABC, test) {
 
